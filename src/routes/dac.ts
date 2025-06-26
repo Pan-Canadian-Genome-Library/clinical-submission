36308d40a6de55b4540cd837156c6f15033c9de9
James Lopez <jamestlopez.code@gmail.com>
2025-06-26 13:37:36 -0400

    fix(dac): remove auth from dac endpoints

--- src/routes/dac.ts
+++ src/routes/dac.ts
@@ -20,7 +20,6 @@
 import express, { json, Router, urlencoded } from 'express';
 
 import dacController from '@/controllers/dacController.js';
-import { authMiddleware } from '@/middleware/auth.js';
 
 export const dacRouter: Router = (() => {
 	const router = express.Router();
@@ -29,10 +28,10 @@ export const dacRouter: Router = (() => {
 
 	// TODO: Change roles
 	router.get('/:dacId', dacController.getDacById);
-	router.get('/', authMiddleware(), dacController.getAllDac);
-	router.post('/create', authMiddleware(), dacController.createDac);
-	router.delete('/:dacId', authMiddleware(), dacController.deleteDac);
-	router.patch('/:dacId', authMiddleware(), dacController.updateDac);
+	router.get('/', dacController.getAllDac);
+	router.post('/create', dacController.createDac);
+	router.delete('/:dacId', dacController.deleteDac);
+	router.patch('/:dacId', dacController.updateDac);
 
 	return router;
 })();
