78f604c49a2d6287cee34e3663cdfaac2960d98b
Azher2Ali <121898125+Azher2Ali@users.noreply.github.com>
2025-09-29 14:41:08 -0400

    Update src/controllers/categoryController.ts
    Co-authored-by: James Lopez <jamestlopez.code@gmail.com>

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -78,7 +78,7 @@ const getCategoryById = validateRequest(getOrDeleteCategoryByID, async (req, res
 		const linkedStudies = await studySvc.getStudiesByCategoryIds([categoryId]);
 
 		if (linkedStudies.length == 0) {
-			logger.info('category is misconfigured, no associated Study');
+			logger.info('Category is misconfigured, no associated study');
 			throw new lyricProvider.utils.errors.NotFound(
 				`category is misconfigured, no associated Study ID - ${categoryId}.`,
 			);
