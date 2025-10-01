93384c53d9e7290e30c99615fbeb4e49be99e721
James Lopez <jamestlopez.code@gmail.com>
2025-10-01 16:53:33 -0400

    fix(submission): if admin skip resource belong check

--- src/controllers/submissionController.ts
+++ src/controllers/submissionController.ts
@@ -386,7 +386,7 @@ const deleteSubmissionById = validateRequest(
 				throw new lyricProvider.utils.errors.BadRequest(`Submission '${submissionId}' not found`);
 			}
 
-			if (authEnabled && user?.username !== submission?.createdBy) {
+			if (authEnabled && (user?.isAdmin || user?.username !== submission?.createdBy)) {
 				throw new lyricProvider.utils.errors.Forbidden('You do not have permission to delete this resource');
 			}
 
@@ -418,7 +418,7 @@ const deleteEntityName = validateRequest(
 				throw new lyricProvider.utils.errors.BadRequest(`Submission '${submissionId}' not found`);
 			}
 
-			if (authEnabled && user?.username !== submission?.createdBy) {
+			if (authEnabled && (user?.isAdmin || user?.username !== submission?.createdBy)) {
 				throw new lyricProvider.utils.errors.Forbidden('You do not have permission to delete this resource');
 			}
 
