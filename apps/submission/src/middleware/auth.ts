c145201adb9d7b987e426831fae74faa592f5679
Leonardo Rivera <leorivera_88@hotmail.com>
2025-10-07 16:40:07 -0400

    update comments

--- apps/submission/src/middleware/auth.ts
+++ apps/submission/src/middleware/auth.ts
@@ -27,8 +27,9 @@ import { extractAccessTokenFromHeader, fetchUserData } from '@/external/pcglAuth
 
 /**
  * Middleware to handle authentication that returns PCGLUserSessionResult to req.user.
- * Only validation is checking if token exists or not, then returned user object/information from authz is added to req.user.
- * To check if a user has permissions to any specific endpoints must be done on the controller level from the passed req.user object
+ * The middleware validates whether a token exists; if valid, the user information returned from the authz service is added to `req.user`.
+ * Optionally, when `requireAdmin` is `true`, the middleware restricts access to admin users only.
+ * Any additional checks for user permissions must be done on the controller level from the passed `req.user` object
  */
 export const authMiddleware = ({ requireAdmin = false }: { requireAdmin?: boolean } = {}) => {
 	const { enabled } = authConfig;
