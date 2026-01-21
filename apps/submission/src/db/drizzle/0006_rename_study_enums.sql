4803a758f71d69a3549773ac37ac2fb48ee3a834
Jon Eubank <joneubank@gmail.com>
2026-01-21 16:52:36 -0500

    Study Enum migration rewritten to use RENAME VALUE

---
+++ apps/submission/src/db/drizzle/0006_rename_study_enums.sql
@@ -0,0 +1,4 @@
+ALTER TYPE "pcgl"."study_context" RENAME VALUE 'CLINICAL' TO 'Clinical';
+ALTER TYPE "pcgl"."study_context" RENAME VALUE 'RESEARCH' TO 'Research';
+ALTER TYPE "pcgl"."study_status" RENAME VALUE 'COMPLETED' TO 'Completed';
+ALTER TYPE "pcgl"."study_status" RENAME VALUE 'ONGOING' TO 'Ongoing';
