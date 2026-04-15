2b804f3fc94fe596c33daf30e1a135a2e46201f9
James Lopez <jamestlopez.code@gmail.com>
2026-04-15 09:09:46 -0400

    feat(study-translation): init

---
+++ apps/submission/src/db/schemas/studyTranslationsSchema.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2025 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { relations } from 'drizzle-orm';
+import { foreignKey, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { pcglSchema } from './generate.js';
+
+export const langagues = pcglSchema.enum('study_status', ['eng_ca', 'fr_ca']);
+
+export const study = pcglSchema.table('study_translations', {
+	study_translation_id: text().primaryKey(),
+	language_id: langagues().notNull(),
+	study_description: text().notNull(), // Assuming the description is large
+	program_name: varchar({ length: 255 }),
+	keywords: text().array(),
+	participant_criteria: text(),
+	funding_sources: text().array().notNull(),
+	created_at: timestamp().notNull().defaultNow(),
+	updated_at: timestamp(),
+});
