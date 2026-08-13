e165c3cce903ffb32b7813eed4bedd7dd76a81c9
James Lopez <jamestlopez.code@gmail.com>
2026-08-13 14:19:31 -0400

    fix: organize shared packages

--- packages/data-model/src/index.ts
+++ packages/data-model/src/index.ts
@@ -17,4 +17,5 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-export * from './types.js';
+export * from './study/study.js'; // Study
+export * from './study/studyTranslations.js'; // Study Translations
